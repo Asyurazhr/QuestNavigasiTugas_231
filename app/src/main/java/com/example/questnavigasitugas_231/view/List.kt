80dae869135941104eb3950151bc6d32089a441e
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-07 21:07:56 +0700

    add button Beranda dengan warna ungu

--- app/src/main/java/com/example/questnavigasitugas_231/view/List.kt
+++ app/src/main/java/com/example/questnavigasitugas_231/view/List.kt
@@ -80,3 +80,13 @@ fun DaftarPeserta(
                 }
             }
             Spacer(modifier = Modifier .height(height = 30.dp))
+            Button(modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id=R.color.purple_500 )
+                ),
+                onClick = onBackToBerandaClick)
+            {
+                Text(text="Beranda")
+            }
